d1c070b578860a9c07cf8a2ce7694cacb79fbe99
Justin <xyz030206@gmail.com>
2017-12-20 21:10:03 +0800

    fix some routes of company pages

--- routes/company.js
+++ routes/company.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/index', function (req, res, next) {
-    res.render('company/company-index', {
+    res.render('company/index', {
         title: '管理頁面'
     });
 });
