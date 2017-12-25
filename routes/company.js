c4c435c0c54ea9d0db62448683d78a44f9fef09b
Justin <xyz030206@gmail.com>
2017-12-25 17:42:51 +0800

    mysql connection syntax

--- routes/company.js
+++ routes/company.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: "scl.mysql.database.azure.com",
+    user: "admin1104@scl",
+    password: 'Qwert@1234',
+    database: 'pmproject',
+    port: 3306,
+});
+
+connection.connect();
 
 /* GET home page. */
 router.get('/index', function (req, res, next) {
@@ -15,9 +25,24 @@ router.get('/crm', function (req, res, next) {
 });
 
 router.get('/ingredient-management', function (req, res, next) {
-    res.render('company/ingredient-management', {
-        title: '原料管理'
+
+    var sql = 'SELECT * FROM product';
+
+    connection.query(sql, function (error, result) {
+        if (error) {
+            throw error;
+        }
+        console.log(result[0]);
     });
+
+    connection.end();
+
+    var data = {
+        ingredient_name: '原料管理',
+        ingredient_price: '677'
+    }
+
+    res.render('company/ingredient-management', data);
 });
 
 router.get('/schedule', function (req, res, next) {
