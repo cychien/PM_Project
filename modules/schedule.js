893f4676115758fe126a064bc44cfd9fec4be3e6
Justin <xyz030206@gmail.com>
2018-01-03 22:33:17 +0800

    fix

--- modules/schedule.js
+++ modules/schedule.js
@@ -8,13 +8,13 @@ var connection = mysql.createConnection({
     port: 3306,
 });
 
-connection.connect();
+// connection.connect();
 
 exports.home = (req, res, next) => {
-	var data = {};
-	
-	var promise = new Promise((resolve, reject) => {
-		var sql = 'SELECT `order`.id as `orderId`,customer.id as customerId,customer.name, GROUP_CONCAT(product.name) as product,\
+    var data = {};
+
+    var promise = new Promise((resolve, reject) => {
+            var sql = 'SELECT `order`.id as `orderId`,customer.id as customerId,customer.name, GROUP_CONCAT(product.name) as product,\
 					sum(include.price*include.qty) as total,\
 					DATE_ADD(`order`.estimated_arrived_date,INTERVAL -1 day) as estimatedShippedDay\
 					FROM pmproject.customer,`order`,include,make,product\
@@ -23,28 +23,28 @@ exports.home = (req, res, next) => {
 					and make.order_id=`order`.id\
 					and product.id = include.product_id\
 					group by `order`.id;'
-		connection.query(sql, (error, result) => {
-        if (error) {
-            reject(error);
-        }
-        var schedules = [];
-		
-		 for (let i = 0; i < result.length; i++) {
-                let schedule = {};
-                ({
-                    orderId: schedule.order_id,
-                    name: schedule.customer_name,
-                    product: schedule.order_product,
-                    total: schedule.order_total,
-                    estimatedShippedDay: schedule.estimated_ship_date
-                } = result[i]);
-                schedules.push(schedule);
-		 }
-			data.order_schedule = schedules;
-            resolve();
-	});
-	
-}).then(() => {
+            connection.query(sql, (error, result) => {
+                if (error) {
+                    reject(error);
+                }
+                var schedules = [];
+
+                for (let i = 0; i < result.length; i++) {
+                    let schedule = {};
+                    ({
+                        orderId: schedule.order_id,
+                        name: schedule.customer_name,
+                        product: schedule.order_product,
+                        total: schedule.order_total,
+                        estimatedShippedDay: schedule.estimated_ship_date
+                    } = result[i]);
+                    schedules.push(schedule);
+                }
+                data.order_schedule = schedules;
+                resolve();
+            });
+
+        }).then(() => {
 
             //先找立即過期的ingredient
             var ingredientSchedules = [];
@@ -55,7 +55,7 @@ exports.home = (req, res, next) => {
 							DATE_ADD(current_date(), interval ((inventory-safe_inventory)/day_demand)+lead_time DAY) as `dateToArrive`\
 							FROM pmproject.ingredient\
 							where inventory<safe_inventory;'
-						   
+
                 connection.query(sql, function (error, result) {
                     if (error) {
                         throw error;
@@ -66,28 +66,28 @@ exports.home = (req, res, next) => {
                         ({
                             name: ingredientSchedule.ingredient_name,
                             inventory: ingredientSchedule.remaining_inventory,
-							eoq:ingredientSchedule.estimated_quantity,
-							dateToPurchase:ingredientSchedule.estimated_order_date,
-							dateToArrive:ingredientSchedule.estimated_arrived_date
-							
+                            eoq: ingredientSchedule.estimated_quantity,
+                            dateToPurchase: ingredientSchedule.estimated_order_date,
+                            dateToArrive: ingredientSchedule.estimated_arrived_date
+
                         } = result[i]);
                         ingredientSchedules.push(ingredientSchedule);
                     }
-					data.ingredient_schedule = ingredientSchedules;
-                   resolve();
+                    data.ingredient_schedule = ingredientSchedules;
+                    resolve();
                 });
             });
         })
-.then(() => {
-			
-			
-			var p = new Promise((resolve, reject) => {
-            var sql = 'SELECT ingredient.id,ingredient.name,inventory,eoq,\
+        .then(() => {
+
+
+            return new Promise((resolve, reject) => {
+                var sql = 'SELECT ingredient.id,ingredient.name,inventory,eoq,\
 							DATE_ADD(current_date(), interval ((inventory-safe_inventory)/day_demand) DAY) as `dateToPurchase`,\
 							DATE_ADD(current_date(), interval ((inventory-safe_inventory)/day_demand)+lead_time DAY) as `dateToArrive`\
 							FROM pmproject.ingredient\
 							where inventory>safe_inventory and ((inventory-safe_inventory)/day_demand)<3;'
-						   
+
                 connection.query(sql, function (error, result) {
                     if (error) {
                         throw error;
@@ -98,22 +98,22 @@ exports.home = (req, res, next) => {
                         ({
                             name: ingredientSchedule.ingredient_name,
                             inventory: ingredientSchedule.remaining_inventory,
-							eoq:ingredientSchedule.estimated_quantity,
-							dateToPurchase:ingredientSchedule.estimated_order_date,
-							dateToArrive:ingredientSchedule.estimated_arrived_date
-							
+                            eoq: ingredientSchedule.estimated_quantity,
+                            dateToPurchase: ingredientSchedule.estimated_order_date,
+                            dateToArrive: ingredientSchedule.estimated_arrived_date
+
                         } = result[i]);
                         data.ingredient_schedule.push(ingredientSchedule);
                     }
-					
+
                     resolve();
-                   
+
                 });
             });
-			
-			})
 
-.then(() => {
+        })
+
+        .then(() => {
             res.render('company/schedule', data);
         });
 }
